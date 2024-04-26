9894f9b1778af74561218bf767aa7eea0be7aaa7
Facundo <facundofigueroa789@gmail.com>
2024-04-26 18:27:12 -0300

    Views for the admin are created, including a blog list, a specific blog, and the home page view is edited to utilize the layout.

---
+++ src/routes/BlogView.jsx
@@ -0,0 +1,52 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+import { baseUrl } from "../config";
+import { Layout } from "../layouts/Layout";
+
+export const BlogView = () => {
+  const [blog, setBlog] = useState({ title: '', description: '', image: '', createdAt: 'new Date()' });
+  const { id } = useParams();
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(`${baseUrl}/blogs/${id}`);
+      const data = await res.json();
+      if (data.status == 404) {
+        return navigate('/')
+      }
+      setBlog(data)
+    }
+    fetchData()
+  }, [])
+
+  return (
+    <Layout>
+      <section className="mx-auto lg:py-20 py-8 sm:py-16 ">
+        <article>
+          <header className="">
+            <h1
+              className="mx-auto sm:px-6 max-w-3xl px-4 font-bold font-heading leading-tighter md:text-5xl text-3xl tracking-tighter mb-6"
+            >
+              {blog.title}
+            </h1>
+          </header>
+          <img
+            src={baseUrl + blog.image}
+            className="mx-auto bg-gray-400 dark:bg-slate-700 lg:max-w-[900px] max-w-full mb-6 sm:rounded-md"
+            alt={blog.title}
+          />
+          <div
+            className="mx-auto sm:px-6 max-w-3xl px-6 mt-8 dark:prose-a:text-blue-400 dark:prose-headings:text-slate-300 dark:prose-invert lg:prose-xl prose prose-a:text-primary prose-headings:font-bold prose-headings:font-heading prose-headings:leading-tighter prose-headings:scroll-mt-[80px] prose-headings:tracking-tighter prose-img:rounded-md prose-img:shadow-lg prose-lg prose-md"
+          >
+            {blog?.description?.split('\n').map((paragraph, i) => <p key={i} className="text-pretty">{paragraph}</p>)}
+          </div>
+        </article>
+      </section>
+
+    </Layout>
+  );
+};
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,24 +1,15 @@
-import {TopNavbar} from "../components/Nav/TopNavbar";
-import {Header} from "../components/Sections/Header";
-import {Services} from "../components/Sections/Services";
-// import {Projects} from "../components/Sections/Projects";
-// import {Blog} from "../components/Sections/Blog";
-// import {Pricing} from "../components/Sections/Pricing";
-import {Contact} from "../components/Sections/Contact";
-import {Footer} from "../components/Sections/Footer"
+import { Layout } from "../layouts/Layout";
+import { Header } from "../components/Sections/Header";
+import { Services } from "../components/Sections/Services";
+import { Contact } from "../components/Sections/Contact";
 
 export const Home = () => {
   return (
-    <>
-      <TopNavbar />
+    <Layout>
       <Header />
       <Services />
-      {/* <Projects /> */}
-      {/* <Blog />
-      <Pricing /> */}
       <Contact />
-      <Footer />
-    </>
+    </Layout>
   );
 }
 
