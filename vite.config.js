e84886dae417da5bed4ccafc2720c54fe19764b6
Facundo <facundofigueroa789@gmail.com>
2024-04-26 18:15:21 -0300

    The fonts are added to index.html, unnecessary Vite configuration is removed, and the image of nogadev's missile is added

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    jsxFactory: 'React.createElement',
-    jsxFragment: 'React.Fragment',
-    jsxInject: `import * as React from 'react'`,
-    jsc: {
-      transform: {
-        react: {
-          throwIfNamespace: false,
-          // Otras opciones de transformación de react...
-        },
-      },
-    },
-  },
+  plugins: [react()]
 })
