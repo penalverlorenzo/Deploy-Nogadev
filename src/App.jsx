a6e13edf83c7ea33e1f18ce20658fc512a4aea14
Facundo <facundofigueroa789@gmail.com>
2024-04-26 18:28:27 -0300

    The views are integrated into App.jsx, and react-router-dom is integrated for handling the routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { Helmet } from "react-helmet";
+import {Route, Routes, BrowserRouter  } from "react-router-dom";
 
 import './App.css'
 import { Home } from './routes/Home'
+import { BlogsView } from "./routes/BlogsView";
+import { BlogView } from "./routes/BlogView";
+import { AdminView } from "./routes/AdminView";
 function App() {
   return (
-    <>
-    <Helmet>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link href="https://fonts.googleapis.com/css2?family=Khula:wght@400;600;800&display=swap" rel="stylesheet" />
-      </Helmet>
-      <Home />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blogs" element={<BlogsView />} />
+        <Route path="/blogs/:id" element={<BlogView />} />
+        <Route path="/admin" element={<AdminView />} />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
