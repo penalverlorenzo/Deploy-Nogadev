60272ae7703b93c9be62b761f9b4aa051e5d2d10
lautarohelp <lautarotoledo391@gmail.com>
2024-04-26 19:46:32 -0300

    se estilo el componente

--- src/components/chatBot/ActionProvider.jsx
+++ src/components/chatBot/ActionProvider.jsx
@@ -1,26 +1,32 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import React from "react";
-
-
+import { GeneratedPromptAnswer } from "./GeneratedPromptAnswer";
 const ActionProvider = ({ createChatBotMessage, setState, children }) => {
   const handleHello = ()=>{
     const botMessage = createChatBotMessage(`Hello. I'm kike Nice to Meet you`)
-    
     setState((prev) => ({
       ...prev,
       messages: [...prev.messages, botMessage],
     }));
   }
+  const generatedPrompt = async(message)=>{
+    const res = await GeneratedPromptAnswer(message)
+    const splite = res.replaceAll('*', ' ')
 
-  const generatedPrompt = ()=>{
-    const botMessage = createChatBotMessage("Response:", {widget: 'generatePromptAnswer', load: true});
+    const regex = /Respuesta(?:\sKike)?:\s*(.*)/g
+
+    const parsedMsg = splite.match(regex);
+    console.log({parsedMsg});
+
+    console.log({splite}); 
+    const respuesta = parsedMsg || splite;
+    const botMessage = createChatBotMessage(respuesta);
     setState((prev) => ({
       ...prev,
       messages: [...prev.messages, botMessage],
     }));
   }
-
   return (
     <div>
       {React.Children.map(children, (child) => {
@@ -34,5 +40,4 @@ const ActionProvider = ({ createChatBotMessage, setState, children }) => {
     </div>
   );
 };
-
 export default ActionProvider;
--- src/components/chatBot/GeneratedPromptAnswer.jsx
+++ src/components/chatBot/GeneratedPromptAnswer.jsx
@@ -1,43 +1,24 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import { useEffect, useState } from "react"
-
-
-export const GeneratedPromptAnswer = ({ prompt }) => {
-    const [result, setResult] = useState('');
-
-    useEffect(() => {
-        async function fetchData(prompt) {
-            try {
-                const response = await fetch('http://localhost:3000/api/v1/info', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ message: prompt })
-                });
-                const data = await response.json();
-                console.log({data});
-                setResult(data);
+// import { useEffect, useState } from "react"
+export const GeneratedPromptAnswer = async (prompt) => {
+    let result;
+    try {
+        const response = await fetch('http://localhost:3000/api/v1/info', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ message: prompt })
+        });
+        const data = await response.json();
+                // console.log({prompt});
+                // console.log({data});
+                result = data;
+                return result.response;
             } catch (error) {
-                console.log(result);
-                setResult({response: `EN: There's something wrong, try sending your message again. ESP: Se ha producido un error, intenta enviar tu mensaje de vuelta`})
+                return  `EN: There's something wrong, try sending your message again. ESP: Se ha producido un error, intenta enviar tu mensaje de vuelta`;
             }
+        
         }
-        fetchData(prompt);
-    }, []);
-    return (<section style={{ display: 'flex' }}>
-        <div className="react-chatbot-kit-chat-bot-avatar">
-            <div className="react-chatbot-kit-chat-bot-avatar-container">
-                <p className="react-chatbot-kit-chat-bot-avatar-letter">B</p>
-            </div>
-        </div>
-        <div className="react-chatbot-kit-chat-bot-message">
-            <span>{result.response ?result.response: result.error }</span>
-            <div className="react-chatbot-kit-chat-bot-message-arrow"></div>
-        </div>
-    </section>
-    )
-        ;
-}
