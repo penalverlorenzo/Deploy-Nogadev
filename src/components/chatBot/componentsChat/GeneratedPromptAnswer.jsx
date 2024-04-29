41cd306b1662ec2da197f6dcc2d96bdc4c2744a7
Lorenzo Peñalver <lpenalver@nogadev.com>
2024-04-29 15:47:52 -0300

    Logica con local storage añadida para mejorar velocidad

--- src/components/chatBot/componentsChat/GeneratedPromptAnswer.jsx
+++ src/components/chatBot/componentsChat/GeneratedPromptAnswer.jsx
@@ -5,20 +5,27 @@
 export const GeneratedPromptAnswer = async (prompt) => {
     let result;
     try {
-        const response = await fetch('http://localhost:3000/api/v1/info', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ message: prompt })
-        });
-        const data = await response.json();
-                // console.log({prompt});
-                // console.log({data});
-                result = data;
-                return result.response;
-            } catch (error) {
-                return  `EN: There's something wrong, try sending your message again. ESP: Se ha producido un error, intenta enviar tu mensaje de vuelta`;
-            }
-        
+        const lowerCasePrompt = prompt.toLowerCase()
+        const localData = localStorage.getItem(`${lowerCasePrompt}`)
+
+        if (!localData) {
+            const response = await fetch('http://localhost:3000/api/v1/info', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ message: prompt })
+            });
+            const data = await response.json();
+            result = data;
+            localStorage.setItem(`${lowerCasePrompt}`, JSON.stringify(data.response))
+        } else {
+            console.log("Si Data");
+            result = localData
         }
+        return result.response ? result.response : result;
+    } catch (error) {
+        return `EN: There's something wrong, try sending your message again. ESP: Se ha producido un error, intenta enviar tu mensaje de vuelta`;
+    }
+
+}
