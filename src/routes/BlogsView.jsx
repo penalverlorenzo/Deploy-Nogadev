fdb34ac56d4bae578a728fee297156dc74269262
Facundo <facundofigueroa789@gmail.com>
2024-05-02 16:27:31 -0300

    Integration of viewTransitionAPI and connection between components (with a blog preview) and full blog (with all content).

--- src/routes/BlogsView.jsx
+++ src/routes/BlogsView.jsx
@@ -1,7 +1,11 @@
 import { BlogList } from "../components/blog/BlogList"
+import { UsePageMetadata } from "../hooks/UsePageMetadata";
 import { Layout } from "../layouts/Layout"
+const titlePage = 'Blog';
+const descriptionPage = "Explore our latest blog posts covering a wide range of topics including technology, lifestyle, travel, and more. Stay informed and inspired with our diverse collection of articles written by experts and enthusiasts.";
 
 export const BlogsView = ()=>{
+  UsePageMetadata(titlePage, descriptionPage)
   return (
     <Layout >
       {/* --------- Blog List-------------- */}
