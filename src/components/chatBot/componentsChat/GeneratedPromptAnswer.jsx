8559f061096a25d9099fbaeabf0743227aa9eb6b
Lorenzo Peñalver <lpenalver@nogadev.com>
2024-05-06 11:08:17 -0300

    Cambio y mejora de parseo de mensaje, ahora cambia las vocales con caracteres especiales por vocales con caracteres simples

--- src/components/chatBot/componentsChat/GeneratedPromptAnswer.jsx
+++ src/components/chatBot/componentsChat/GeneratedPromptAnswer.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-control-regex */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
@@ -6,11 +7,22 @@
 import { generateToken } from "./AccessToken"
 
 const parseMessage = (message) => {
-    const lowerCasePrompt = message.toLowerCase()
-    const noQuestionMark = lowerCasePrompt.replaceAll('?',"")
-    const noInitialQuestionMark = noQuestionMark.replaceAll('¿',"")
-    const deletedSpaces = noInitialQuestionMark.replaceAll(' ',"")
-    return deletedSpaces
+    // Expresiones Regulares para cada posible caso de vocales con caracteres especiales
+    const vowelCaseA = /[áÁàâÀÂäÄãÃ]/g;
+    const vowelCaseE = /[èéÈêÊëËÉ]/g;
+    const vowelCaseI = /[îíÍÎïÌìÏ]/g;
+    const vowelCaseO = /[öóòÒôõÕÔÓÖ]/g;
+    const vowelCaseU = /[üÜùûÛÙúÚ]/g;
+    // Se limpia el mensaje, se lo pasa a minúsculas, se le quitan los espacios y signos de preguntas, además se reemplazan las vocales con caracteres especiales por vocales simples
+    const cleanedMessage = message.toLowerCase()
+    .replaceAll(/[¿?\s]/g, '')
+    .replaceAll(vowelCaseA, 'a')
+    .replaceAll(vowelCaseI, 'i')
+    .replaceAll(vowelCaseO, 'o')
+    .replaceAll(vowelCaseU, 'u')
+    .replaceAll(vowelCaseE, 'e');
+
+    return cleanedMessage
 }
 
 
@@ -22,7 +34,7 @@ export const GeneratedPromptAnswer = async (prompt) => {
     generateToken()
     try {
         if (!localData) {
-            
+
             const response = await fetch('http://localhost:3000/api/v1/info', {
                 method: 'POST',
                 headers: {
@@ -37,12 +49,12 @@ export const GeneratedPromptAnswer = async (prompt) => {
                 localStorage.removeItem(`${parseMessage}`)
             }
             localStorage.setItem(`${parsedData}`, JSON.stringify(data.response))
-        } 
+        }
         else {
             result = JSON.parse(localData)
         }
         const finalRes = result.response ? result.response : result;
-        console.log({finalRes, result});
+        console.log({ finalRes, result });
         return finalRes
     } catch (error) {
         return `EN: There's something wrong, try sending your message again. ESP: Se ha producido un error, intenta enviar tu mensaje de vuelta`;
