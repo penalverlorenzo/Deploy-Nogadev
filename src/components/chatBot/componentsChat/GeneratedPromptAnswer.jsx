a14614f4482bf9ff361b30a6a2524038e5fb7eb4
Lorenzo Peñalver <lpenalver@nogadev.com>
2024-05-08 12:40:12 -0300

    Limpieza de codigo, el parseo del prompt fue llevado al backend

--- src/components/chatBot/componentsChat/GeneratedPromptAnswer.jsx
+++ src/components/chatBot/componentsChat/GeneratedPromptAnswer.jsx
@@ -5,64 +5,30 @@
 
 import { dbURL } from "../../../config";
 import { refreshToken } from "./AccessToken";
-import { createItem, getItem } from "./redisRequests";
-
-const parseMessage = (message) => {
-    // Expresiones Regulares para cada posible caso de vocales con caracteres especiales
-    const vowelCaseA = /[áÁàâÀÂäÄãÃ]/g;
-    const vowelCaseE = /[èéÈêÊëËÉ]/g;
-    const vowelCaseI = /[îíÍÎïÌìÏ]/g;
-    const vowelCaseO = /[öóòÒôõÕÔÓÖ]/g;
-    const vowelCaseU = /[üÜùûÛÙúÚ]/g;
-    // Se limpia el mensaje, se lo pasa a minúsculas, se le quitan los espacios y signos de preguntas, además se reemplazan las vocales con caracteres especiales por vocales simples
-    const cleanedMessage = message.toLowerCase()
-    .replaceAll(/[¿?\s]/g, '')
-    .replaceAll(vowelCaseA, 'a')
-    .replaceAll(vowelCaseI, 'i')
-    .replaceAll(vowelCaseO, 'o')
-    .replaceAll(vowelCaseU, 'u')
-    .replaceAll(vowelCaseE, 'e');
-
-    return cleanedMessage
-}
 
 export const GeneratedPromptAnswer = async (prompt) => {
     let result;
     let data;
-    // const localData = localStorage.getItem(`${parsedData}`) 
-    const parsedData = parseMessage(prompt)
     const localStorageToken = localStorage.getItem('token')
-    const doesItemExist = await getItem(parsedData,dbURL,localStorageToken)
-    console.log({doesItemExist});
     try {
-        if (!doesItemExist) {
-                    const response = await fetch(dbURL+'/info', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'Authorization': `Bearer ${localStorageToken}`
-                        },
-                        body: JSON.stringify({ message: prompt })
-                    });
-                    data = await response.json(); 
-                    result = data;
-                        if (response.status === 401) {
-                            await refreshToken();
-                            result = await GeneratedPromptAnswer(prompt);
-                        }
-                if (result === '' || result.response === '') {
-                    result = await GeneratedPromptAnswer('Toma esta pregunta, reformulala, luego devuelve la respuesta sin devolver la reformulación de la respuesta, solo me interesa la respuesta en sí: '+ prompt)
-                }
-                await createItem(parsedData,result.response? result.response : result,dbURL, localStorageToken)
-                // localStorage.setItem(`${parsedData}`, JSON.stringify(result.response? result.response : result))
-
-            
+        const response = await fetch(dbURL + '/info', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorageToken}`
+            },
+            body: JSON.stringify({ message: prompt })
+        });
+        data = await response.json();
+        result = data;
+        if (response.status === 401) {
+            await refreshToken();
+            result = await GeneratedPromptAnswer(prompt);
         }
-        else {
-            result = doesItemExist
+        if (result === '' || result.response === '') {
+            result = await GeneratedPromptAnswer('Toma esta pregunta, reformulala, luego devuelve la respuesta sin devolver la reformulación de la respuesta, solo me interesa la respuesta en sí: ' + prompt)
         }
         let finalRes = result.response ? result.response : result;
-
         return finalRes
     } catch (error) {
         return `EN: There's something wrong, try sending your message again. ESP: Se ha producido un error, intenta enviar tu mensaje de vuelta`;
