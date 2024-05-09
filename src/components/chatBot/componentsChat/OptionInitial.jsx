669bd12ec7aa3553d4283c1412ca0be574f2490b
Lorenzo Peñalver <lpenalver@nogadev.com>
2024-05-09 13:13:15 -0300

    Cambios de colores de chatbot, añadida altura del chatbot

--- src/components/chatBot/componentsChat/OptionInitial.jsx
+++ src/components/chatBot/componentsChat/OptionInitial.jsx
@@ -19,7 +19,7 @@ export const OptionInitial = ({props, lang}) => {
   
   return (
     <>
-      <p className="text-sm text-white text-center mb-1.5">{(lang === "ES")? PES : PEN}</p>
+      <p className="text-sm text-black text-center mb-1.5">{(lang === "ES")? PES : PEN}</p>
       <div className="flex ">
         <button className=" bg-black text-white mr-1 py-1 px-2 rounded-2xl text-[13px] shadow-md transition-all hover:shadow-gray-400/45 " onClick={() => generatedPrompt((lang === "ES")? contactES : contactEN)}>{(lang === "ES")? "Contactos" : "Contacts"}</button>
         <button className=" bg-black text-white mr-1 py-1 px-2 rounded-2xl text-[13px] shadow-md transition-all hover:shadow-gray-400/45  " onClick={() => generatedPrompt((lang === "ES")? objectivesES : objectivesEN)} >{(lang === "ES")? "Objetivos" : "Objectives"}</button>
