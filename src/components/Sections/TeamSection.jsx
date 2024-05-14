bbaaf8cae56bbe5791d7c0d5ed356c785b00ca86
Facundo <facundofigueroa789@gmail.com>
2024-05-14 15:16:44 -0300

    Abstracción de la secciónd eteam a otro componente

---
+++ src/components/Sections/TeamSection.jsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import { Team } from '../Elements/Team';
+import { useLanguageSections } from '../../hooks/useLanguageSections';
+
+export const TeamSection = () => {
+  const team = useLanguageSections('team');
+
+
+  return (
+    <div className="">
+      <div className="container">
+        <Advertising className="flex flex-col items-center gap-8">
+          <h2 className="text-4xl extraBold mx-auto text-center">{team.title}</h2>
+          <p className="text-2xl font-medium mx-auto text-center">
+            {team.description}
+          </p>
+          <Team />
+        </Advertising>
+      </div>
+    </div>
+  );
+};
+
+const Advertising = styled.div`
+  margin: 80px 0; 
+  position: relative;
+  @media (max-width: 1160px) {
+    padding: 100px 0 40px 0;
+  }
+  @media (max-width: 860px) {
+    flex-direction: column;
+    padding: 0 0 30px 0;
+    margin: 80px 0 0px 0;
+  }
+`;
