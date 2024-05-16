b37af3441e72a55b9b9b9e48b511131ca74afec0
Facundo <facundofigueroa789@gmail.com>
2024-05-16 09:44:00 -0300

    se crea una utilidad para hacer notificaciones de loading, error o success de las solicitudes

---
+++ src/utils/notify.js
@@ -0,0 +1,31 @@
+import { toast } from "react-toastify";
+
+let toastPosition = window.innerWidth < 641 ? "top-right" : "bottom-right";
+export const notify = (message, typeOperation, time) => {
+  toast[typeOperation](message, {
+    position: toastPosition,
+    autoClose: time || 1500,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
+};
+
+export const loadingNotify = (message) => {
+  const idToast = toast.loading(message, {
+    position: toastPosition,
+    autoClose: 1500,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
+  setTimeout(() => {
+    toast.dismiss(idToast);
+  }, 750);
+};
