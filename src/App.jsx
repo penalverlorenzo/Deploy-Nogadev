2e88022b743b8645a49d49163e91542094ba3d21
Facundo <facundofigueroa789@gmail.com>
2024-05-16 12:00:03 -0300

    Se comenta todo lo relacionado a blogs para no exponer rutas que no son funcionales en producción

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import {Route, Routes, BrowserRouter  } from "react-router-dom";
 
 import { Home } from './routes/Home'
 // import { BlogsView } from "./routes/BlogsView";
-import { BlogView } from "./routes/BlogView";
-import { AdminView } from "./routes/AdminView";
+// import { BlogView } from "./routes/BlogView";
+// import { AdminView } from "./routes/AdminView";
 import { ContactView } from "./routes/ContactView";
 import { ThanksView } from "./routes/ThanksView";
 
@@ -13,8 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/blogs" element={<BlogsView />} /> */}
-        <Route path="/blogs/:id" element={<BlogView />} />
-        <Route path="/admin" element={<AdminView />} />
+        {/* <Route path="/blogs/:id" element={<BlogView />} /> */}
+        {/* <Route path="/admin" element={<AdminView />} /> */}
         <Route path="/contact" element={<ContactView />} />
         <Route path="/thanks" element={<ThanksView />}/>
       </Routes>
