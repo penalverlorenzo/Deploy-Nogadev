90cf3acccba4b4ee48628fe7979a5fd81d700c25
Facundo <facundofigueroa789@gmail.com>
2024-05-17 18:14:53 -0300

    Se intega manejo de errores, se crea el estado que habilita el botón de submit del formulario y se integra el redirectOnFinish en el hook

--- src/hooks/UseForm.jsx
+++ src/hooks/UseForm.jsx
@@ -1,50 +1,58 @@
-import { useState } from "react"
-import { loadingNotify, notify, stopNotify } from "../utils/notify"
+import { useState } from "react";
+import { loadingNotify, notify, stopNotify } from "../utils/notify";
+import { useNavigate } from "react-router-dom";
 
 export const useForm = ({
   initialValues,
-  onSubmit
+  onSubmit,
+  redirectOnFinish = null,
 }) => {
-  const [formData, setFormData] = useState(initialValues)
-  const [loading, setLoading] = useState(false)
+  const [formData, setFormData] = useState(initialValues);
+  const [loading, setLoading] = useState(false);
+  const [enabledSubmit, setEnabledSubmit] = useState(true);
   // const [errors, setErrors] = useState(initialErrors)
-
+  const navigate = useNavigate();
   const handleChange = ({ name, value }) => {
     // console.log({name, value});
-    if ((!name || name === undefined) && (!value || value === undefined)) return
-    setFormData(prevState => {
-      console.log({prevState});
-      return {...prevState, [name]: value}
+    if ((!name || name === undefined) && (!value || value === undefined))
+      return;
+    setFormData((prevState) => {
+      console.log({ prevState });
+      return { ...prevState, [name]: value };
     });
-  }
-  
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setEnabledSubmit(false);
     // estado de carga true
     // validar errores
     // si los errores existen terminar el loading
     // ejecutar el onsubmit y retornar la respuesta
     // el loader se frena
-    const id = loadingNotify('Sending mail');
-    const data = await onSubmit();
+    const id = loadingNotify("Sending mail");
 
-    stopNotify(id);
-
-    if (data?.status === 'error') {
-      notify('Error submitting form, please try again later', 'error');
-    } else {
-      notify('Submitted successfully!', 'success');
-      // navigate('/thanks');
+    try {
+      await onSubmit();
+      notify("Submitted successfully!", "success");
+      if (redirectOnFinish) {
+        navigate(redirectOnFinish);
+      }
+      setFormData(initialValues);
+    } catch (error) {
+      notify("Error submitting form, please try again later", "error");
+    } finally {
+      setEnabledSubmit(true);
+      stopNotify(id);
     }
-    setFormData(initialValues);
   };
-  
+
   return {
     formData,
     loading,
     setLoading,
     handleChange,
-    handleSubmit
-  }
-}
+    handleSubmit,
+    enabledSubmit,
+  };
+};
