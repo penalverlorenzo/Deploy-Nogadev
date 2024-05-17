f9f90709a3e8441d4c68bb744d97442dab1ac42e
Lorenzo Peñalver <lpenalver@nogadev.com>
2024-05-17 12:56:13 -0300

    Cambio de Icono de Chatbot por uno con menor resolución

--- src/components/chatBot/componentsChat/FlightBotAvatar.jsx
+++ src/components/chatBot/componentsChat/FlightBotAvatar.jsx
@@ -1,12 +1,13 @@
 
-import img from '../../../assets/img/nogadevIcon.jpg'
+// import img from '../../../assets/img/nogadevIcon.jpg'
+import lowResImg from '../../../assets/img/lowResIcon.jpg'
 import LazyImageLoader from '../../LazyLoading/LazyLoadingContainer'
 export const FlightBotAvatar = () => {
   
   return (
     <div className="size-10 mr-3 ">
       <LazyImageLoader>
-      <img className="rounded-t-full rounded-r-full object-cover" src={img} alt="" />
+      <img className="rounded-t-full rounded-r-full" src={lowResImg} alt="" />
       </LazyImageLoader>
     </div>
   )
