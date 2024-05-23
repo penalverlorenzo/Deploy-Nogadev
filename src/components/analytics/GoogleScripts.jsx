6e336aba8038316a8786e737a71822fe56fcd4b0
Facundo <facundofigueroa789@gmail.com>
2024-05-23 16:34:40 -0300

    Se modifican los scripts de analytics, ads y tag manager.Se inyectan en el main.jsx

--- src/components/analytics/GoogleScripts.jsx
+++ src/components/analytics/GoogleScripts.jsx
@@ -1,40 +1,35 @@
+/* eslint-disable no-undef */
 import { useEffect } from 'react';
 
 export const GoogleScripts = () => {
   useEffect(() => {
-    const addScript = (src, innerHTML = '') => {
-      const script = document.createElement('script');
-      script.async = true;
-      script.src = src;
-      if (innerHTML) script.innerHTML = innerHTML;
-      document.head.appendChild(script);
-      return script;
-    };
+    const script1 = document.createElement('script');
+    const script2 = document.createElement('script');
+    const script3 = document.createElement('script');
+    
+    script1.src = 'https://www.googletagmanager.com/gtag/js?id=AW-16535222825';
+    script1.async = true;
+    document.body.appendChild(script1);
+    script2.src = 'https://www.googletagmanager.com/gtag/js?id=G-YJD660BYT5';
+    script2.async = true;
+    document.body.appendChild(script2);
+    script3.src = 'https://www.googletagmanager.com/gtag/js?id=G-16VSH8QX0Z';
+    script3.async = true;
+    document.body.appendChild(script3);
 
-    const script1 = addScript('https://www.googletagmanager.com/gtag/js?id=AW-16535222825');
-    const script2 = addScript('', `
-      window.dataLayer = window.dataLayer || [];
-      function gtag() { dataLayer.push(arguments); }
-      gtag('js', new Date());
-      gtag('config', 'AW-16535222825');
-    `);
-    const script3 = addScript('https://www.googletagmanager.com/gtag/js?id=G-YJD660BYT5');
-    const script4 = addScript('', `
-      window.dataLayer = window.dataLayer || [];
-      function gtag() { dataLayer.push(arguments); }
-      gtag('js', new Date());
-      gtag('config', 'G-YJD660BYT5');
-    `);
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
 
+    gtag('config', 'AW-16535222825');
+    gtag('config', 'G-YJD660BYT5');
+    gtag('config', 'G-16VSH8QX0Z');
+    console.log(window.dataLayer);
     return () => {
-      document.head.removeChild(script1);
-      document.head.removeChild(script2);
-      document.head.removeChild(script3);
-      document.head.removeChild(script4);
+      document.body.removeChild(script1);
+      document.body.removeChild(script2);
     };
   }, []);
 
   return null;
 };
-
-export default GoogleScripts;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { GoogleScripts } from './components/analytics/GoogleScripts.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <GoogleScripts />
+
   </React.StrictMode>,
 )
